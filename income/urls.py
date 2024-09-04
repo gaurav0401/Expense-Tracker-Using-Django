3931d951c967ca309ef915106d89a6b5d66bff1a
gaurav bhatt <gaurav.tnv6970@gmail.com>
2024-09-04 14:30:11 +0530

    Debugging

--- income/urls.py
+++ income/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name="income"),
     path('add-income', views.add_income, name="add-income"),
     path('edit-income/<int:id>', views.income_edit, name="income-edit"),
+    path('/search-income', csrf_exempt(views.search_income), name="search_income"),
     path('income-delete/<int:id>', views.delete_income, name="income-delete"),
-    path('search-income', csrf_exempt(views.search_income), name="search_income")
+   
 ]
